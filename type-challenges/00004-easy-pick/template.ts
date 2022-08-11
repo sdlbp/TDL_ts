97fcbff448b34b86e79234ec16b9eefb86114ba7
lbp <lbpapp@163.com>
2022-08-11 11:07:57 +0800

    error: pick

--- type-challenges/00004-easy-pick/template.ts
+++ type-challenges/00004-easy-pick/template.ts
@@ -2,7 +2,16 @@ type MyPick<T, K extends keyof T> = {
     [key in K]: T[key]
 }
 
+
 // pick 代表从一个已有数据取出想要的字段，构建出新的类型
 
 // 泛型的约束 extends关键字 - https://www.typescriptlang.org/docs/handbook/2/generics.html#generic-constraints
 // 遍历 in 关键字 - https://www.typescriptlang.org/docs/handbook/2/mapped-types.html
+
+/*
+错误1：
+type MyPick<T, K> = {
+  K in keyof T ? U : T[K]
+}
+
+*/
